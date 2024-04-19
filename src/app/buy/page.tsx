7b10feb24865540b0efd7188adc7813243a7901c
Gustavo Pedrini Maestri <gustavo.maestri@ae.studio>
2024-04-18 21:02:25 -0300

    added toasts to comunicate with user and custom ammount to buy credits

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -3,13 +3,19 @@
 import { CheckIcon } from "@heroicons/react/20/solid";
 import Button from "@/components/Button";
 import { useChain } from "@/contexts/Chain";
-import { toWei, prepareContractCall, sendAndConfirmTransaction } from "thirdweb";
+import {
+  toWei,
+  prepareContractCall,
+  sendAndConfirmTransaction,
+} from "thirdweb";
 import { useActiveAccount } from "thirdweb/react";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
 type Tier = {
   name: string;
   id: string;
-  price: number;
+  price?: string;
   description: string;
   features: string[];
 };
@@ -18,14 +24,14 @@ const tiers: Tier[] = [
   {
     name: "Basic",
     id: "tier-basic",
-    price: 5,
+    price: "5",
     description: "The essentials to validate some proofs",
     features: ["Cheapest plan", "Up to 1,000 inputs aprox"],
   },
   {
     name: "Advanced",
     id: "tier-advanced",
-    price: 15,
+    price: "15",
     description: "A pack that can get you a long way.",
     features: [
       "Most popular",
@@ -36,7 +42,7 @@ const tiers: Tier[] = [
   {
     name: "Professional",
     id: "tier-professional",
-    price: 30,
+    price: "30",
     description: "A pack for heavy users.",
     features: [
       "Up to 6,000 inputs aprox",
@@ -44,27 +50,38 @@ const tiers: Tier[] = [
       "Can handle super complex proofs",
     ],
   },
+  {
+    name: "Custom",
+    id: "tier-custom",
+    description: "Get what you want.",
+    features: [
+      "Any ammount of inputs and runs",
+      "Exactly what you need",
+      "Can handle super complex proofs",
+    ],
+  },
 ];
 
 export default function BuyCreditsPage() {
-  const { client, escrowContract, tokenContract, fetchEscrowBalance } = useChain();
+  const [customPrice, setCustomPrice] = useState<string>("100");
+  const { client, escrowContract, tokenContract, fetchEscrowBalance } =
+    useChain();
   const activeAccount = useActiveAccount();
 
   const handleBuy = async (tier: Tier) => {
-    if (!client || !escrowContract || !tokenContract) {
+    if (!client || !escrowContract || !tokenContract || !activeAccount) {
       return;
     }
 
     try {
-      if (!activeAccount) return;
-
+      toast.info("Approving token ammount...");
       const approveTx = prepareContractCall({
         contract: tokenContract,
         // Pass the method signature that you want to call
         method: "function approve(address spender, uint256 amount)",
         // and the params for that method
         // Their types are automatically inferred based on the method signature
-        params: [escrowContract?.address, toWei((tier?.price || 5).toString())],
+        params: [escrowContract?.address, toWei(tier?.price || "5")],
       });
 
       await sendAndConfirmTransaction({
@@ -72,28 +89,38 @@ export default function BuyCreditsPage() {
         account: activeAccount,
       });
 
+      toast.success("Token ammount approved!");
+    } catch (e) {
+      console.log("error", e);
+      toast.error("Error on token ammount approval, " + e);
+    }
+
+    try {
+      toast.info("Depositing credits...");
       const depositTx = prepareContractCall({
         contract: escrowContract,
         // Pass the method signature that you want to call
         method: "function deposit(uint256 amount)",
         // and the params for that method
         // Their types are automatically inferred based on the method signature
-        params: [toWei((tier?.price || 5).toString())],
+        params: [toWei(tier?.price || "5")],
       });
 
       await sendAndConfirmTransaction({
         transaction: depositTx,
         account: activeAccount,
       });
-      
+
+      toast.success("Credit deposit successfull!");
       fetchEscrowBalance();
     } catch (e) {
       console.log("error", e);
+      toast.error("Error on credits deposit, " + e);
     }
   };
 
   return (
-    <div className="bg-gray-900 py-12 sm:py-24">
+    <div className="bg-gray-900 py-12 sm:py-24 h-screen">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-4xl text-center">
           <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
@@ -105,11 +132,11 @@ export default function BuyCreditsPage() {
           and give you some kind of advantage.
         </p>
 
-        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto py-10 flex w-full flex-wrap gap-8 items-center justify-center">
           {tiers.map((tier) => (
             <div
               key={tier.id}
-              className={`ring-1 ring-white/10 rounded-3xl p-8 xl:p-10`}
+              className={`ring-1 ring-white/10 rounded-3xl p-8 xl:p-10 w-full md:w-1/2 lg:w-1/3 h-[400px]`}
             >
               <div className="flex items-center justify-between gap-x-4">
                 <h3
@@ -123,9 +150,24 @@ export default function BuyCreditsPage() {
                 {tier.description}
               </p>
               <p className="mt-6 flex items-baseline gap-x-1">
-                <span className="text-4xl font-bold tracking-tight text-white">
-                  {tier?.price}$
-                </span>
+                {tier?.price ? (
+                  <span className="text-4xl font-bold tracking-tight text-white">
+                    $ {tier?.price}
+                  </span>
+                ) : (
+                  <>
+                    <span className="text-4xl font-bold tracking-tight text-white ml-2">
+                      $
+                    </span>
+                    <input
+                      type="number"
+                      id="custom-price"
+                      value={customPrice}
+                      onChange={(e) => setCustomPrice(e.target.value)}
+                      className="w-full -ml-8 pl-8 pr-2 pb-1 pt-1.5 h-12 text-[34px] text-white bg-transparent text-xl text-bold border-white border-2 rounded-lg focus-visible:outline-none"
+                    />
+                  </>
+                )}
               </p>
 
               <Button
@@ -134,7 +176,7 @@ export default function BuyCreditsPage() {
                 label="Buy credits"
                 className="w-full mt-4"
                 onClick={() => {
-                  handleBuy(tier);
+                  handleBuy({ ...tier, price: tier?.price || customPrice });
                 }}
               />
               <ul
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import Header from "@/components/Header";
 import Providers from "@/utils/Providers";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +27,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
+          <ToastContainer theme="dark" />
         </body>
       </html>
     </Providers>
